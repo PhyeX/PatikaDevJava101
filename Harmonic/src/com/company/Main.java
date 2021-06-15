8c91c81e07325f2153d1e7dba5bff058e4b226d8
PhyeX <45934056+PhyeX@users.noreply.github.com>
2021-06-16 00:47:47 +0300

    Add files via upload

---
+++ Harmonic/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("N sayısını giriniz :");
+        double n = input.nextInt();
+        double result = 0;
+        while (n > 0) {
+            result += (1 / n);
+            n--;
+        }
+
+        System.out.println(result);
+        
+    }
+}
