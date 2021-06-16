d76d07f97e85dd9795da66aac84783b69900658d
PhyeX <45934056+PhyeX@users.noreply.github.com>
2021-06-17 00:06:26 +0300

    Update Main.java

--- PatikaDevTaksi/src/com/company/Main.java
+++ PatikaDevTaksi/src/com/company/Main.java
@@ -6,7 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+  	Scanner scan = new Scanner(System.in);
+        int km;
+        double perKm = 2.20, total, startPrice = 10;
+        System.out.println("Km giriniz : ");
+        km = scan.nextInt();
+        total = (km * perKm);
+        total += startPrice;
+        total = (total < 20) ? 20 : total;
+        System.out.println("Toplam tutar : " + total);
 
 
     }
