27b4a910b7acf13acf707616b046cce0b6195308
PhyeX <45934056+PhyeX@users.noreply.github.com>
2021-06-16 18:08:48 +0300

    Add files via upload

---
+++ Fibonacci/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(fibo(7));
+    }
+    static int fibo(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
