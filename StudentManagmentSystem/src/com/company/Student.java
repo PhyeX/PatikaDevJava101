7ca28ac57b7801c15a7e348e08cdc9389856fce3
PhyeX <45934056+PhyeX@users.noreply.github.com>
2021-06-17 00:57:17 +0300

    Add files via upload

---
+++ StudentManagmentSystem/src/com/company/Student.java
@@ -0,0 +1,96 @@
+package com.company;
+
+public class Student {
+    String name,stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+
+
+    Student(String name, int classes, String stuNo, Course mat,Course fizik,Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+
+    public void addBulkExamNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+
+    }
+
+    public void addBulkVerbalNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.verbalNote = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.verbalNote = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.verbalNote = kimya;
+        }
+
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0 || this.mat.verbalNote == 0 || this.fizik.verbalNote == 0 || this.kimya.verbalNote == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+        this.avarage =
+                ( (this.fizik.note + this.kimya.note + this.mat.note) / 3 * 0.8 )
+                +( ( this.fizik.verbalNote + this.kimya.verbalNote + this.mat.verbalNote) / 3 * 0.2 )
+
+        ;
+    }
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void printNote(){
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.mat.note);
+        System.out.println("Fizik Notu : " + this.fizik.note);
+        System.out.println("Kimya Notu : " + this.kimya.note);
+        System.out.println("Matematik Sözlü : " + this.mat.verbalNote);
+        System.out.println("Fizik Sözlü : " + this.fizik.verbalNote);
+        System.out.println("Kimya Sözlü : " + this.kimya.verbalNote);
+    }
+
+}
